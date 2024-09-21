370ea2005b1932915b68eb8abfeed5c4e13ec988
Ian Paul <iandouglaspaul@gmail.com>
2024-09-22 00:27:38 +0800

    update to plotly app

--- plotly_app/app.py
+++ plotly_app/app.py
@@ -2,9 +2,11 @@
 # visit http://127.0.0.1:8050/ in your web browser.
 
 
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, dash_table
 import plotly.express as px
 import pandas as pd
+
+from points_by_hand import DealtHand
 
 app = Dash(__name__)
 
@@ -18,10 +20,12 @@ app = Dash(__name__)
 
 # df = pd.read_csv("csv_outputs/crib_hands_output.csv")
 # df = pd.read_parquet("crib_hands_output_smol.parquet")
-df = pd.read_parquet("csv_outputs/crib_hands_output_big_boi.parquet")
+# df = pd.read_parquet("csv_outputs/crib_hands_output_big_boi.parquet")
 # df = pd.read_csv("crib_hands_output_smol.csv")
+df = pd.read_parquet("../csv_outputs/crib_hands_sorted.parquet")
 
-df = df.head(20)
+# df = df.head(20)
+
 
 # should sort input string to make sure it matches one of the hands
 def sort_hand(hand: str) -> str:
@@ -29,13 +33,32 @@ def sort_hand(hand: str) -> str:
     cards.sort()
     return ",".join(cards)
 
-hand = "2H,3H,4H,5H,6H,8H"
-sorted_hand = sort_hand(hand)
 
-row_of_df = df.loc[df["dealt_hand_sorted_str"] == sorted_hand]
+# hand = "2H,3H,4H,5H,6H,8H"
+hand = "5S,3C,8D,5C,9H,AH"
+hand_obj = DealtHand(hand, df)
+
+# hand_df = hand_obj.get_df()
+# row_of_hand_df = df.iloc[12]
+# row_of_hand_df = df.loc[df["cut_card"] == "TD"]
+# sorted_hand = sort_hand(hand)
+
+# row_of_df = df.loc[df["dealt_hand_sorted_str"] == sorted_hand]
 
 # fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-fig = px.bar(df, x="dealt_hand", y="max_hand_max_points")
+# fig = px.bar(hand_obj.to_dict(), x="cut_card")
+
+hand_obj_dict = hand_obj.to_dict()
+hand_obj_dict_list = list(hand_obj_dict.items())
+
+hand_records = hand_obj.get_df().to_dict("records")
+print(hand_records)
+print([{"name": i, "id": i} for i in hand_obj.to_dict().keys()])
+
+dt = dash_table.DataTable(
+    hand_records,
+    [{"name": i, "id": i} for i in hand_obj_dict.keys()],
+)
 
 app.layout = html.Div(
     children=[
@@ -45,11 +68,13 @@ app.layout = html.Div(
         Dash: A web application framework for your data.
     """
         ),
-        dcc.Graph(id="example-graph", figure=fig),
-        html.Div(children=row_of_df["dealt_hand"]),
-        html.Div(children=row_of_df["max_hand"]),
-        html.Div(children=row_of_df["max_hand_max_points"]),
-        html.Div(children=row_of_df["max_hand_ev_points"]),
+        # dcc.Graph(id="example-graph", figure=fig),
+        # html.Div(children=row_of_hand_df),
+        # html.Div(children=row_of_df["dealt_hand"]),
+        # html.Div(children=row_of_df["max_hand"]),
+        # html.Div(children=row_of_df["max_hand_max_points"]),
+        # html.Div(children=row_of_df["max_hand_ev_points"]),
+        html.Div(children=dt),
     ]
 )
 
