d54c963d6d93b1ab81c5ee5c935fbf74b6541c61
Ian Paul <iandouglaspaul@gmail.com>
2024-11-12 20:43:15 +0700

    refactor & add duckdb

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,8 +1,34 @@
 import streamlit as st
 import pandas as pd
+from io import StringIO
 import numpy as np
+import duckdb
 
 st.title("Uber pickups in NYC")
+
+
+# @st.cache_resource
+def get_db_connection():
+    if "duck_conn" not in st.session_state:
+        st.session_state["duck_conn"] = duckdb.connect(":memory:")
+
+    return st.session_state["duck_conn"]
+
+
+def main():
+    conn = get_db_connection()
+    # create_side_bar(conn)
+    create_page(conn)
+
+
+def create_page(conn: duckdb.DuckDBPyConnection):
+
+    # Create a text element and let the reader know the data is loading.
+    data_load_state = st.text("Loading data...")
+    # Load 10,000 rows of data into the dataframe.
+    data = load_data(10000)
+    # Notify the reader that the data was successfully loaded.
+    data_load_state.text("Done! (using st.cache_data)")
 
 
 @st.cache_data
@@ -18,9 +44,20 @@ def load_data(nrows):
     return df
 
 
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text("Loading data...")
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text("Done! (using st.cache_data)")
+def load_files(conn: duckdb.DuckDBPyConnection, files: list):
+    for file in files:
+        stringio = StringIO(file.getvalue().decode("utf-8"))
+
+        if file.name.endswith(".csv"):
+            conn.read_csv(stringio).to_table(file.name[:-4])
+
+
+if __name__ == "__main__":
+    main()
+
+    # # Create a text element and let the reader know the data is loading.
+    # data_load_state = st.text("Loading data...")
+    # # Load 10,000 rows of data into the dataframe.
+    # data = load_data(10000)
+    # # Notify the reader that the data was successfully loaded.
+    # data_load_state.text("Done! (using st.cache_data)")
